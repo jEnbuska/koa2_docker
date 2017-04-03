a4c17d81498c610200aeba889da5afe2b45a8ddd
jenbuska <joona.enbuska@eficode.com>
2017-04-03 20:59:39 +0300

    use logger

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 const Koa = require('koa');
 const app = new Koa();
 app.use(require('koa-bodyparser')());
+app.use(require('./logger').default);
+
+
 const api = require('./api/v1');
 app.use(api.routes());
 
