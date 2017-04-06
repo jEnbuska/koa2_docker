1892f1d2ba28df85709c1636b9187ede646aebff
jenbuska <joona.enbuska@eficode.com>
2017-04-06 20:41:11 +0300

    create auth controller

---
+++ src/app/controllers/auth.js
@@ -0,0 +1,25 @@
+const admin = require('firebase-admin');
+const {firebaseConfig, shared_secret} = require('../config');
+const app = admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig),
+  databaseURL: "https://reactcourse-83808.firebaseio.com"
+});
+
+const getToken = async (ctx) => {
+  try{
+    app.auth().createCustomToken(shared_secret)
+      .then(function(customToken) {
+        ctx.body = customToken;
+        ctx.status = 200;
+      })
+      .catch(function(error) {
+        ctx.status = 500;
+        ctx.body = error;
+      });
+  }catch(e){
+    ctx.status = 500;
+    console.error(e.message);
+  }
+};
+
+module.exports = {getToken};
