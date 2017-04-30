fdea9568da3b1ef3ccde03a9c90882c0f243f1b3
jenbuska <joona.enbuska@eficode.com>
2017-04-30 19:56:30 +0300

    make random errors more sparce

--- src/app/random-error.js
+++ src/app/random-error.js
@@ -1,6 +1,8 @@
+
+let i = 1;
 export default async (ctx, next) => {
   const random = Math.random()*20;
-  if(random>18){
+  if(i++%100 === 0){
     ctx.status = 500;
     ctx.body = "Random error";
   }else{
